1c7d3525d89df359138258e830d5ea485ee8aa54
jtopyuebi <classicmcnet@163.com>
2025-04-11 23:50:41 +0800

    更新代理设置界面和逻辑
    在 `WndProc` 函数中添加了字体、静态文本控件、组合框和按钮的创建代码。组合框的端口选项文本由 "10001"、"10002"、"10003" 修改为 "线路一"、"线路二"、"线路三"。更新了按钮和描述文本以反映新的选项，并在按钮点击事件中根据选择更新代理地址和描述文本。

--- proxy/proxy.cpp
+++ proxy/proxy.cpp
@@ -57,69 +57,68 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     switch (message)
     {
     case WM_CREATE:
-    {
-        // 创建字体
-        hFont = CreateFont(
-            20,                    // 字体高度
-            0,
-            0,
-            0,
-            FW_NORMAL,
-            FALSE,
-            FALSE,
-            FALSE,
-            DEFAULT_CHARSET,
-            OUT_DEFAULT_PRECIS,
-            CLIP_DEFAULT_PRECIS,
-            CLEARTYPE_QUALITY,
-            DEFAULT_PITCH | FF_SWISS,
-            _T("Microsoft YaHei")
-        );
+        {
+            // 创建字体
+            hFont = CreateFont(
+                20,                    // 字体高度
+                0,
+                0,
+                0,
+                FW_NORMAL,
+                FALSE,
+                FALSE,
+                FALSE,
+                DEFAULT_CHARSET,
+                OUT_DEFAULT_PRECIS,
+                CLIP_DEFAULT_PRECIS,
+                CLEARTYPE_QUALITY,
+                DEFAULT_PITCH | FF_SWISS,
+                _T("Microsoft YaHei")
+            );
 
-        // 创建静态文本控件
-        HWND hStaticText = CreateWindow(
-            _T("STATIC"), _T("将网络连接至："),
-            WS_CHILD | WS_VISIBLE | SS_LEFT,
-            30, 15, 120, 30,  // 文本控件的位置和大小
-            hWnd, NULL,
-            ((LPCREATESTRUCT)lParam)->hInstance, NULL);
-        SendMessage(hStaticText, WM_SETFONT, (WPARAM)hFont, TRUE);
+            // 创建静态文本控件
+            HWND hStaticText = CreateWindow(
+                _T("STATIC"), _T("将网络连接至："),
+                WS_CHILD | WS_VISIBLE | SS_LEFT,
+                30, 15, 120, 30,  // 文本控件的位置和大小
+                hWnd, NULL,
+                ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+            SendMessage(hStaticText, WM_SETFONT, (WPARAM)hFont, TRUE);
 
-        // 创建组合框
-        hCombo = CreateWindow(_T("COMBOBOX"), NULL,
-            WS_VISIBLE | WS_CHILD | CBS_DROPDOWNLIST,
-            150, 15, 100, 90,  // 调整组合框的位置以适应新添加的文本控件
-            hWnd, (HMENU)IDC_COMBOBOX,
-            ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+            // 创建组合框
+            hCombo = CreateWindow(_T("COMBOBOX"), NULL,
+                WS_VISIBLE | WS_CHILD | CBS_DROPDOWNLIST,
+                150, 15, 100, 90,  // 调整组合框的位置以适应新添加的文本控件
+                hWnd, (HMENU)IDC_COMBOBOX,
+                ((LPCREATESTRUCT)lParam)->hInstance, NULL);
 
-        SendMessage(hCombo, WM_SETFONT, (WPARAM)hFont, TRUE);
+            SendMessage(hCombo, WM_SETFONT, (WPARAM)hFont, TRUE);
 
+            // 添加端口选项
+            SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)_T("线路一"));
+            SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)_T("线路二"));
+            SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)_T("线路三"));
+            SendMessage(hCombo, CB_SETCURSEL, 0, 0); // 默认选中第一个
+            hButton = CreateWindow(
+                _T("BUTTON"),
+                _T("启用代理"),
+                WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | BS_FLAT,
+                35, 55, 207, 40,  // 按钮pos
+                hWnd,
+                (HMENU)IDC_BUTTON,
+                ((LPCREATESTRUCT)lParam)->hInstance,
+                NULL);
+            SendMessage(hButton, WM_SETFONT, (WPARAM)hFont, TRUE);
 
-        // 添加端口选项
-        SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)_T("10001"));
-        SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)_T("10002"));
-        SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)_T("10003"));
-        SendMessage(hCombo, CB_SETCURSEL, 0, 0); // 默认选中第一个
-        hButton = CreateWindow(
-            _T("BUTTON"),
-            _T("启用代理"),
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | BS_FLAT,
-            35, 55, 207, 40,  // 按钮pos
-            hWnd,
-            (HMENU)IDC_BUTTON,
-            ((LPCREATESTRUCT)lParam)->hInstance,
-            NULL);
-        SendMessage(hButton, WM_SETFONT, (WPARAM)hFont, TRUE);
-
-        hDesc = CreateWindow(
-            _T("STATIC"), _T("此连接较为稳定，速度适中"),
-            WS_CHILD | WS_VISIBLE | SS_CENTER,
-            0, 105, 280, 40,
-            hWnd, (HMENU)IDC_STATIC_DESC,
-            ((LPCREATESTRUCT)lParam)->hInstance, NULL);
-        SendMessage(hDesc, WM_SETFONT, (WPARAM)hFont, TRUE);
-    }
-    break;
+            hDesc = CreateWindow(
+                _T("STATIC"), _T("此连接较为稳定，速度适中"),
+                WS_CHILD | WS_VISIBLE | SS_CENTER,
+                0, 105, 280, 40,
+                hWnd, (HMENU)IDC_STATIC_DESC,
+                ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+            SendMessage(hDesc, WM_SETFONT, (WPARAM)hFont, TRUE);
+        }
+        break;
 
     case WM_PAINT:
     {
@@ -140,40 +139,50 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     break;
 
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDC_BUTTON)
+case WM_COMMAND:
+    if (LOWORD(wParam) == IDC_BUTTON)
+    {
+        TCHAR szPort[6] = { 0 };
+        TCHAR szProxy[32] = _T("10.88.202.71:");
+
+        // 获取当前选中的端口
+        int nIndex = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+        SendMessage(hCombo, CB_GETLBTEXT, nIndex, (LPARAM)szPort);
+
+        if (_tcscmp(szPort, _T("线路一")) == 0) {
+            _tcscpy_s(szPort, _T("10001"));
+        }
+        else if (_tcscmp(szPort, _T("线路二")) == 0) {
+            _tcscpy_s(szPort, _T("10002"));
+        }
+        else if (_tcscmp(szPort, _T("线路三")) == 0) {
+            _tcscpy_s(szPort, _T("10003"));
+        }
+
+        _tcscat_s(szProxy, szPort); // 拼接完整代理地址
+
+        proxyEnabled = !proxyEnabled;
+        if (SetSystemProxy(proxyEnabled, szProxy))
         {
-            TCHAR szPort[6] = { 0 };
-            TCHAR szProxy[32] = _T("10.88.202.71:");
-
-            // 获取当前选中的端口
-            int nIndex = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
-            SendMessage(hCombo, CB_GETLBTEXT, nIndex, (LPARAM)szPort);
-
-            _tcscat_s(szProxy, szPort); // 拼接完整代理地址
-
-            proxyEnabled = !proxyEnabled;
-            if (SetSystemProxy(proxyEnabled, szProxy))
-            {
-                SetWindowText(hButton, proxyEnabled ? _T("禁用代理") : _T("启用代理"));
-            }
+            SetWindowText(hButton, proxyEnabled ? _T("禁用代理") : _T("启用代理"));
         }
-        else if (LOWORD(wParam) == IDC_COMBOBOX && HIWORD(wParam) == CBN_SELCHANGE) {
-            TCHAR szPort[6] = { 0 };
-            int nIndex = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
-            SendMessage(hCombo, CB_GETLBTEXT, nIndex, (LPARAM)szPort);
+    }
+    else if (LOWORD(wParam) == IDC_COMBOBOX && HIWORD(wParam) == CBN_SELCHANGE) {
+        TCHAR szPort[6] = { 0 };
+        int nIndex = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+        SendMessage(hCombo, CB_GETLBTEXT, nIndex, (LPARAM)szPort);
 
-            if (_tcscmp(szPort, _T("10001")) == 0) {
-                SetWindowText(hDesc, _T("此连接较为稳定，速度适中"));
-            }
-            else if (_tcscmp(szPort, _T("10002")) == 0) {
-                SetWindowText(hDesc, _T("此连接拥有外网访问权限"));
-            }
-            else if (_tcscmp(szPort, _T("10003")) == 0) {
-                SetWindowText(hDesc, _T("此速度极快的代理，但有时不可用"));
-            }
+        if (_tcscmp(szPort, _T("线路一")) == 0) {
+            SetWindowText(hDesc, _T("此连接较为稳定，速度适中"));
         }
-        break;
+        else if (_tcscmp(szPort, _T("线路二")) == 0) {
+            SetWindowText(hDesc, _T("此连接拥有外网访问权限"));
+        }
+        else if (_tcscmp(szPort, _T("线路三")) == 0) {
+            SetWindowText(hDesc, _T("此速度极快的代理，但有时不可用"));
+        }
+    }
+    break;
 
     case WM_SETCURSOR:
         if (LOWORD(lParam) == HTCLIENT && ((HWND)wParam) == hStatic) {
